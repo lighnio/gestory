090b3154153a5c917a6307dfb5a23d3f1cbc3a88
ripterdust <bryantello2010@hotmail.com>
2021-12-04 23:46:21 -0600

    Starting to build the navbar

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -9,7 +9,9 @@ router
         if(req.session.loggedIn){
             res.render('index', {
                 login: true,
-                name: req.session.data.name
+                name: req.session.data.name,
+                rol: req.session.data.rol,
+                user: req.session.data.user
             })
         }else{
             res.redirect('/login')
