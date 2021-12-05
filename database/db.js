c4c032ae7fd1138619693cc83b4e188a219074e6
ripterdust <bryantello2010@hotmail.com>
2021-12-05 14:42:41 -0600

    Used .env

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'login'
+    database: process.env.DB_NAME
 });
 
 connection.connect(err => {
