2d71fd4d106a1df4d5df537d90499da879e4d5a2
ripterdust <bryantello2010@hotmail.com>
2021-12-05 17:21:54 -0600

    Refactoring made

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -64,4 +64,50 @@ accountController.registerPost = async (req, res) => {
     });
     
 }
+
+accountController.auth = async (req, res) => {
+    const {user, pass} = req.body;
+    if(user && pass){
+        connection.query('SELECT * FROM users WHERE user = ?', [user], async(err, results) => {
+            if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+                res.render('login', {
+                    alert: true,
+                    alertTitle: 'Error',
+                    alertMessage: 'Username or password incorrect',
+                    alertIcon: 'error',
+                    showConfirmButton: true,
+                    time: 13000,
+                    ruta: '/login'
+                })
+            }else{
+                req.session.loggedIn = true;
+                const {name, rol, user} = results[0]
+                req.session.data = {
+                    name, rol, user
+                }
+                res.render('login', {
+                    alert: true,
+                    alertTitle: 'Success',
+                    alertMessage: 'Login success',
+                    alertIcon: 'success',
+                    showConfirmButton: false,
+                    time: 1500,
+                    ruta: '/'
+                })
+            }
+        });
+    }else{
+        res.render('login', {
+            alert: true,
+            alertTitle: 'Error',
+            alertMessage: 'Plese type your username or pasword',
+            alertIcon: 'warning',
+            showConfirmButton: false,
+            time: 1500,
+            ruta: '/login'
+        })
+    }
+    
+};
+
 module.exports = accountController;
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {loginView, registerView, logOut, accountView, registerPost} = require('./../controllers/accountController');
+const {loginView, registerView, logOut, accountView, registerPost, auth} = require('./../controllers/accountController');
 const { indexView } = require('../controllers/dashboardController');
 
 // Routing
@@ -12,50 +12,7 @@ router
     .get('/logout', logOut)
     .get('/account', accountView)
     .post('/register', registerPost)
-    .post('/auth', async (req, res) => {
-        const {user, pass} = req.body;
-        if(user && pass){
-            connection.query('SELECT * FROM users WHERE user = ?', [user], async(err, results) => {
-                if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
-                    res.render('login', {
-                        alert: true,
-                        alertTitle: 'Error',
-                        alertMessage: 'Username or password incorrect',
-                        alertIcon: 'error',
-                        showConfirmButton: true,
-                        time: 13000,
-                        ruta: '/login'
-                    })
-                }else{
-                    req.session.loggedIn = true;
-                    const {name, rol, user} = results[0]
-                    req.session.data = {
-                        name, rol, user
-                    }
-                    res.render('login', {
-                        alert: true,
-                        alertTitle: 'Success',
-                        alertMessage: 'Login success',
-                        alertIcon: 'success',
-                        showConfirmButton: false,
-                        time: 1500,
-                        ruta: '/'
-                    })
-                }
-            });
-        }else{
-            res.render('login', {
-                alert: true,
-                alertTitle: 'Error',
-                alertMessage: 'Plese type your username or pasword',
-                alertIcon: 'warning',
-                showConfirmButton: false,
-                time: 1500,
-                ruta: '/login'
-            })
-        }
-        
-    })
+    .post('/auth', auth)
     
     .get('*', (req, res) => {
         res.redirect('/')
