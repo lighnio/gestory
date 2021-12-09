daaa38b58e45e39d755a3906de2786efa366cb25
ripterdust <bryantello2010@hotmail.com>
2021-12-08 23:00:33 -0600

    fixed morgan bug

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
 const session = require('express-session');
-const morgan = require('morgan');
 
 // Settings
 app.set('port', 3000 || process.env.PORT);
@@ -16,13 +15,13 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use('/resources', express.static('public'));
-app.use('/resources', express.static(path.join(__dirname, 'public')));
+app.use('/resources', express.static(path.join(__dirname, 'public')));                                          
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
-                                                                                                            
+
 // routes
 app.use('/', require('./router/mainRouter'))
 
