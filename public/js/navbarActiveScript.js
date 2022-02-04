fa1d0c07f7a65d5ecaa9ab8b400ecbcc2ec6957f
ripterdust <bryantello2010@hotmail.com>
2022-02-04 15:36:08 -0600

    Added custom navigation navbar color

---
+++ public/js/navbarActiveScript.js
@@ -0,0 +1,11 @@
+let navItems = document.querySelectorAll('.navItem');
+
+let { pathname : path } = window.location;
+
+navItems.forEach(element => {
+    element.classList.remove('active');
+    
+    if(element.id == path){
+        element.classList.add('active');
+    }
+})
