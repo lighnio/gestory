0a0d236551189d6aa00665426ac486d49a863056
ripterdust <bryantello2010@hotmail.com>
2022-02-24 15:00:26 -0600

    Renaming the files

---
+++ app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import session from 'express-session';
+import morgan from 'morgan';
+
+
+// Settings
+export const app = express();
+app.set('port', process.env.PORT || 3000);
+    dotenv.config({
+        path: './env/.env'
+    })
+
+// Middlewares
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(path.join(__dirname, 'public')));                                          
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(morgan('tiny'));
+
+// routes
+app.use('/api/', require('./router/apiRouter'))
+app.use('/', require('./router/mainRouter'));
+
+
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { app } from './app';
+
+
+
+app.listen(app.get('port'), () => console.log(`Server running on port ${app.get('port')}`));
