df793b87b7cbf7d0febcbfc78907c1b7d4fa0764
ripterdust <bryantello2010@hotmail.com>
2022-02-24 23:21:21 -0600

    Configured the fake command to run ts

--- src/fakeData/fake.js
+++ src/fakeData/fake.js
@@ -1,68 +1,43 @@
-const {name, lastName, password, username, email } = require('minifaker');
-const credentials = require('../env/credentials.js');
-    /* Credentials Structure */
-    /*
-        module.exports = {
-        DB_HOST: '',
-        DB_USER: '',
-        DB_PASSWORD: '',
-        DB_NAME: ''
+"use strict";
+exports.__esModule = true;
+var _a = require('minifaker'), name = _a.name, lastName = _a.lastName, password = _a.password, username = _a.username, email = _a.email;
+var db_1 = require("../database/db");
+/* Credentials Structure */
+/*
+    module.exports = {
+    DB_HOST: '',
+    DB_USER: '',
+    DB_PASSWORD: '',
+    DB_NAME: ''
 }
-    */
-
-require('minifaker/locales/en'); 
-
-
-
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: credentials.DB_HOST,
-    user: credentials.DB_USER,
-    password: credentials.DB_PASSWORD,
-    database: credentials.DB_NAME,
-    multipleStatements: true
-});
-
-
-connection.connect(err => {
-    if(err){
-        console.log(`Connection error: ${err}`)
+*/
+require('minifaker/locales/en');
+console.log('faker');
+var data = [];
+var fill = function (amount) {
+    if (amount <= 0) {
         return;
     }
-    return 'Connection succesfully'
-});
-
-
-
-// const connection = require('../database/db.js');
-// const Connection = require('mysql/lib/Connection');
-
-let data = []
-const fill = (amount) => {
-    if(amount <= 0){
-        return;
-    }else{
+    else {
         var dataGen = {
-            firstName: `${name()} ${lastName()}`,
+            firstName: "".concat(name(), " ").concat(lastName()),
             user: username(),
             pass: password(),
             mail: email(),
-            rol: amount % 2 == 0 ? 'admin': 'data entry'
-        }
+            rol: amount % 2 == 0 ? 'admin' : 'data entry'
+        };
         // console.log('data: ', dataGen)
         fill(amount - 1);
-        
     }
     data.push(dataGen);
-}
+};
 fill(process.argv[2]);
 // console.log(data);
-
-for(let i in data){
-    connection.query('INSERT INTO users SET ?', {
-        name: data[i].firstName, 
+for (var i in data) {
+    db_1.connection.query('INSERT INTO users SET ?', {
+        name: data[i].firstName,
         user: data[i].user,
-        pass: data[i].pass, 
+        pass: data[i].pass,
         mail: data[i].mail,
         rol: data[i].rol
     });
