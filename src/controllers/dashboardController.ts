7eb2667d7fa598635bc0eb173cae38481a4ca7ea
ripterdust <bryantello2010@hotmail.com>
2022-03-02 15:09:48 -0600

    Deleted the products controller in dashboardController.ts

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -70,31 +70,6 @@ export const salesById = (req: Request, res: Response) => {
 
 // This get all products
 
-export const products = (req: Request, res: Response) => {
-    if (req.session.loggedIn) {
-        const fields: string =
-            'BIN_TO_UUID( idProduct ) AS idProduct, productName, serialNumber, productPrice, productCategory';
-        const query: string = `SELECT ${fields} from products`;
-
-        connection.query(query, async (err, results) => {
-            if (err) throw err;
-
-            // @ts-ignore
-            const { rol } = req.session.data;
-
-            const data = {
-                rol,
-                products: results,
-                total: results.length,
-            };
-
-            res.render('products', data);
-        });
-    } else {
-        res.redirect('/');
-    }
-};
-
 // This function returns all the users
 export const manageUsers = (req: Request, res: Response) => {
     if (req.session.loggedIn) {
