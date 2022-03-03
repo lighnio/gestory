6988f88d111ef607d191ff91fb58be65fd52daa1
ripterdust <bryantello2010@hotmail.com>
2022-03-03 14:19:42 -0600

    Finished the multer configuration

--- src/multer/multerConfig.ts
+++ src/multer/multerConfig.ts
@@ -1,3 +1,15 @@
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
+
+const diskStorage = multer.diskStorage({
+    destination: path.join(__dirname, '../images'),
+    filename: (req, file, cb) => {
+        let uniqueName = `${Date.now()}-ecommerce-${file.originalname}`;
+        cb(null, uniqueName);
+    },
+});
+
+export const fileupload = multer({
+    storage: diskStorage,
+}).single('image');
