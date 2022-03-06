58e257ef3851e660feeeb1433a11d0ec9636c1ac
ripterdust <bryantello2010@hotmail.com>
2022-03-05 21:06:56 -0600

    Finished to process the product info

--- src/helpers/api/getProductHelper.ts
+++ src/helpers/api/getProductHelper.ts
@@ -1,7 +1,25 @@
 import { productType } from '../../types/apiType';
 // This helper process the product and converts the img to a base64 img
 export const processProductHelper = (product: productType) => {
-    const { id } = product;
+    const {
+        id,
+        productCategory,
+        productImage,
+        productName,
+        productPrice,
+        serialNumber,
+        purchasePrice,
+    } = product;
 
-    return { id };
+    const data: productType = {
+        id,
+        productCategory,
+        productName,
+        productPrice,
+        serialNumber,
+        purchasePrice,
+        productImage: Buffer.from(productImage).toString('base64'),
+    };
+
+    return data;
 };
