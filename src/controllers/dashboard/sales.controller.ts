48032f723b12a3b0cdd03bbd2a21dff9a148000f
ripterdust <bryantello2010@hotmail.com>
2022-03-14 17:30:29 -0600

    Added the helpers and express-session-mys types

--- src/controllers/dashboard/sales.controller.ts
+++ src/controllers/dashboard/sales.controller.ts
@@ -2,60 +2,50 @@ import { salesByIdHelper } from '../../helpers/saleById';
 import { salesHelper } from '../../helpers/salesHelper';
 import { connection } from '../../database/db';
 import { Request, Response } from 'express';
+import { getQuery } from '../../helpers/getSalesQuery';
 
 // THis is the main view and it retuns all the sales
 export const indexView = async (req: Request, res: Response) => {
     if (req.session.loggedIn) {
         // @ts-ignore
         const { name, rol, user } = req.session.data;
-        const {date} = req.query;
-        const datequery = date? `WHERE dateSale BETWEEN '${date}-01-01' AND '${date}-12-31'` : '';
-        
+        const { date } = req.query;
 
         const currencyPrefix = 'Q';
-        
-        const queryAll: string =
-            `SELECT BIN_TO_UUID(idSale) AS idSale, saleProfit, dateSale, BIN_TO_UUID(costumerId) AS costumerId FROM sales ${datequery} ORDER BY dateSale DESC LIMIT 0, 10`;
-        const querySum: string = `SELECT COUNT(*) AS COUNT FROM sales ${datequery}`;
-        const queryProfits: string =
-            `SELECT ROUND(SUM(saleProfit), 2) as profits FROM sales ${datequery}`;
-        const queryAvg: string = `SELECT ROUND(AVG(saleProfit), 2) AS avgSum FROM sales ${datequery}`;
 
-        connection.query(
-            `${queryAll};${queryProfits};${querySum};${queryAvg}`,
-            [1, 2, 3, 4],
-            async (err, results) => {
-                if (err) throw err;
+        const query = getQuery(date);
 
-                const {
-                    sales: allSales,
-                    profits: profitObj,
-                    count: total,
-                    avgSum: averageSum,
-                } = salesHelper(results);
+        connection.query(query, [1, 2, 3, 4], async (err, results) => {
+            if (err) throw err;
 
-                const pageName = 'sales'
+            const {
+                sales: allSales,
+                profits: profitObj,
+                count: total,
+                avgSum: averageSum,
+            } = salesHelper(results);
 
-                const { profits } = profitObj;
-                const { avgSum } = averageSum;
+            const pageName = 'sales';
 
-                const creationDate = new Date();
-                const auxdate = date? date : creationDate.getFullYear();
-                res.render('index', {
-                    login: true,
-                    name: name,
-                    rol: rol,
-                    user: user,
-                    total,
-                    allSales,
-                    profits,
-                    avgSum,
-                    auxdate,
-                    currencyPrefix,
-                    pageName
-                });
-            }
-        );
+            const { profits } = profitObj;
+            const { avgSum } = averageSum;
+
+            const creationDate = new Date();
+            const auxdate = date ? date : creationDate.getFullYear();
+            res.render('index', {
+                login: true,
+                name: name,
+                rol: rol,
+                user: user,
+                total,
+                allSales,
+                profits,
+                avgSum,
+                auxdate,
+                currencyPrefix,
+                pageName,
+            });
+        });
     } else {
         res.redirect('/login');
     }
@@ -89,11 +79,11 @@ export const downloadTicket = (req: Request, res: Response) => {
 // This return the order by date
 
 export const dateSales = async (req: Request, res: Response) => {
-    const {date} = req.body;
+    const { date } = req.body;
 
     res.redirect(`/?date=${date}`);
 };
 
-export const page = async(req: Request, res: Response) => {
-    const {page} = req.body;
-}
+export const page = async (req: Request, res: Response) => {
+    const { page } = req.body;
+};
