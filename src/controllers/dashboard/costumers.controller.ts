6f5650b49ac7bef3b2bf576bc935d4d203712448
ripterdust <bryantello2010@hotmail.com>
2022-03-15 11:05:14 -0600

    Imported the helper

--- src/controllers/dashboard/costumers.controller.ts
+++ src/controllers/dashboard/costumers.controller.ts
@@ -1,6 +1,7 @@
 import { connection } from '../../database/db';
 import { formatData } from '../../helpers/manageCostumersHelper';
 import { Request, Response } from 'express';
+import { getQueryForCostumersHelper } from '../../helpers/costumers/getQueryHelper';
 
 // This is a route to manage the costumers
 export const manageCostumers = (req: Request, res: Response) => {
@@ -8,6 +9,7 @@ export const manageCostumers = (req: Request, res: Response) => {
         // @ts-ignore
         const { rol } = req.session.data;
         if (rol == 'admin') {
+            getQueryForCostumersHelper();
             let selectedFields: string = `BIN_TO_UUID(costumerId) AS costumerId, costumerUsername, costumerMail, costumerName`;
             let query: string = `SELECT ${selectedFields} FROM costumers;`;
 
