8675ee3a9c94abe076bc0d7c2386f2f882db820d
ripterdust <bryantello2010@hotmail.com>
2022-03-15 11:08:01 -0600

    Added the total of costumers

--- src/helpers/costumers/getQueryHelper.ts
+++ src/helpers/costumers/getQueryHelper.ts
@@ -1,6 +1,6 @@
 export const getQueryForCostumersHelper = () => {
     let selectedFields: string = `BIN_TO_UUID(costumerId) AS costumerId, costumerUsername, costumerMail, costumerName`;
     let allCostumers: string = `SELECT ${selectedFields} FROM costumers;`;
-
-    return `${allCostumers}`;
+    let avgCostumers: string = 'SELECT COUNT(*) AS total FROM costumers;';
+    return `${allCostumers}${avgCostumers}`;
 };
