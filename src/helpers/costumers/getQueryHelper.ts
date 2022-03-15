736b215c9f68f584a28d65a21bad99ad6809a44c
ripterdust <bryantello2010@hotmail.com>
2022-03-15 11:26:00 -0600

    Fixed the query

--- src/helpers/costumers/getQueryHelper.ts
+++ src/helpers/costumers/getQueryHelper.ts
@@ -1,5 +1,5 @@
 export const getQueryForCostumersHelper = () => {
-    let selectedFields: string = `BIN_TO_UUID(costumerId) AS costumerId, costumerUsername, costumerMail, costumerName`;
+    let selectedFields: string = `BIN_TO_UUID(costumerId) AS costumerId, costumerUsername, costumerMail, costumerName, costumerZipCode, costumerCountry, costumerAdress`;
     let allCostumers: string = `SELECT ${selectedFields} FROM costumers;`;
     let avgCostumers: string = 'SELECT COUNT(*) AS total FROM costumers;';
     return `${allCostumers}${avgCostumers}`;
--- src/helpers/manageCostumersHelper.ts
+++ src/helpers/manageCostumersHelper.ts
@@ -1,6 +1,5 @@
 export const formatData = (results: any) => {
     let [costumers, count] = JSON.parse(JSON.stringify(results));
-    console.log(count);
     let [Count] = count;
     let { total } = Count;
     return { costumers, total };
