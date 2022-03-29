5f5f25738eb69b3e06c99bd4a02809411c09dd24
ripterdust <bryantello2010@hotmail.com>
2022-03-29 13:40:05 -0600

    Added the login query and main login

--- src/controllers/api/costumer.controller.ts
+++ src/controllers/api/costumer.controller.ts
@@ -4,7 +4,19 @@ import { Costumer } from '../../models/Costumer';
 
 class Auth {
     index(req: Request, res: Response) {
-        res.send('Login');
+        const { email, password }: { email: string; password: string } =
+            req.body;
+
+        const query: string = `SELECT * FROM costumers WHERE costumerMail = '${email}';`;
+        connection.query(query, (err, costumer) => {
+            if (err) res.send('An error has ocurred');
+            if (!err) {
+                if (costumer.length > 0) {
+                    res.send(costumer);
+                }
+                if (costumer.length <= 0) res.send('No costumer found');
+            }
+        });
     }
 
     store(req: Request, res: Response) {
