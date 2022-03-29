11da63847d4c3bad3361d43f155061901f625cd1
ripterdust <bryantello2010@hotmail.com>
2022-03-29 12:31:50 -0600

    Added the index method for products

--- src/controllers/api/products.controller.ts
+++ src/controllers/api/products.controller.ts
@@ -1,7 +1,40 @@
 import { Request, Response } from 'express';
+import { connection } from '../../database/db';
+import { processProductHelper } from '../../helpers/api/getProductHelper';
+import { ResponseFormat, productType } from '../../types/apiType';
 
 class Products {
-    index(req: Request, res: Response) {}
+    index(req: Request, res: Response) {
+        const fields: string =
+            'BIN_TO_UUID(idProduct) AS id, productName, serialNumber, productPrice, productCategory, productImage';
+        const query: string = `SELECT ${fields} FROM products ORDER BY idProduct DESC LIMIT 8`;
+        connection.query(query, (err, results) => {
+            if (err) {
+                let response: ResponseFormat = {
+                    err: true,
+                    msg: err.code,
+                };
+
+                res.send(response);
+
+                throw err;
+            }
+
+            let formatedResults: Array<object> = [];
+
+            results.map((product: productType) => {
+                formatedResults = [
+                    ...formatedResults,
+                    processProductHelper(product),
+                ];
+            });
+
+            res.send({
+                err: false,
+                data: formatedResults,
+            });
+        });
+    }
 }
 
 export const products = new Products();
