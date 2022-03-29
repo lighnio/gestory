50e4d150c9b9beb738ccf1d82477d5a15f71829b
ripterdust <bryantello2010@hotmail.com>
2022-03-29 12:37:15 -0600

    Added the category controller

--- src/controllers/api/category.controller.ts
+++ src/controllers/api/category.controller.ts
@@ -1,3 +1,42 @@
-class Category {}
+import { Request, Response } from 'express';
+import { connection } from '../../database/db';
+import { ResponseFormat } from '../../types/apiType';
+
+class Category {
+    index(req: Request, res: Response) {
+        // Getting the category
+        const { category } = req.params;
+
+        if (!category) {
+            let response: ResponseFormat = {
+                err: true,
+                msg: 'No category was specified',
+            };
+            res.send(response);
+        } else {
+            let fields =
+                'BIN_TO_UUID(idProduct) AS idProductd, serialNumber, productPrice, productCategory, productImage';
+            let query = `SELECT ${fields} FROM products WHERE productCategory = '${category}'`;
+
+            connection.query(query, (err, results) => {
+                if (err) {
+                    let response: ResponseFormat = {
+                        err: true,
+                        msg: err.code,
+                    };
+
+                    res.send(response);
+                } else {
+                    let response: ResponseFormat = {
+                        err: false,
+                        data: results,
+                    };
+
+                    res.send(response);
+                }
+            });
+        }
+    }
+}
 
 export const category = new Category();
