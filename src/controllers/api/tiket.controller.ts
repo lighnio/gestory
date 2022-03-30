784dff2968415b7ec0b0dd8d1ebdf95188120518
ripterdust <bryantello2010@hotmail.com>
2022-03-29 20:40:05 -0600

    Added the query

--- src/controllers/api/tiket.controller.ts
+++ src/controllers/api/tiket.controller.ts
@@ -5,7 +5,12 @@ class Tiket {
     async show(req: Request, res: Response) {
         const { id } = req.params;
 
-        res.send('Tiket');
+        const fields = 'ticket';
+        const query: string = `SELECT ${fields} FROM sales WHERE BIN_TO_UUID(idSale) = '${id}';`;
+        console.log(query);
+        connection.query(query, async (err, tiket) => {
+            res.send(tiket);
+        });
     }
 }
 
