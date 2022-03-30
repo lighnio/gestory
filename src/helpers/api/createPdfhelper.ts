42203deb5174a1f47d01291458491b978b20051d
lighnio <charlesvas16@outlook.com>
2022-03-30 00:52:17 -0600

    created file for pdf helper

---
+++ src/helpers/api/createPdfhelper.ts
@@ -0,0 +1,12 @@
+// Imports
+
+
+export const createPDFHelper = (idProduct: Number, quantity : Number) => {
+    let getProduct = `SELECT * FROM products WHERE idProduct = ${idProduct}`
+
+    //procesar venta
+    // Obtener idProduct, Nombre, purchasePrice, 
+    
+    //crear pdf
+
+}
