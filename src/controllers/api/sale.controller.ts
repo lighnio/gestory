b18058aabe6bbdd7eb4315077fe146276f800e07
ripterdust <bryantello2010@hotmail.com>
2022-03-29 20:11:57 -0600

    Added the show controller

--- src/controllers/api/sale.controller.ts
+++ src/controllers/api/sale.controller.ts
@@ -4,6 +4,10 @@ class Sale {
     store(req: Request, res: Response) {
         res.send(req.body);
     }
+
+    show(req: Request, res: Response) {
+        res.send('Ticket');
+    }
 }
 
 export const sale = new Sale();
