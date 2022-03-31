382820fa2901f531e8803435625315a719b6a422
ripterdust <bryantello2010@hotmail.com>
2022-03-31 14:02:10 -0600

    Fixed the nodemailer

--- src/node-mailer/nodeMailer.ts
+++ src/node-mailer/nodeMailer.ts
@@ -1,3 +1,22 @@
 import nodemailer from 'nodemailer';
+import { mailConfig } from './mail.config';
 
-export const transporterd = nodemailer.
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: mailConfig.mail,
+        pass: mailConfig.pass,
+    },
+});
+
+const mailOptions = {
+    from: 'bryantello2010@hotmail.com',
+    to: 'bryantello2010@hotmail.com',
+    subject: 'Prueba',
+    text: 'COrreo enviado desde nodejs',
+};
+
+transporter.sendMail(mailOptions, (err) => {
+    if (err) return console.log(err);
+    console.log('Enviado');
+});
