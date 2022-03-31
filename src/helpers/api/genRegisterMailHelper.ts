ebee3133b6c1af7b3c360752e57133e9eabd1ef3
ripterdust <bryantello2010@hotmail.com>
2022-03-31 14:38:13 -0600

    Fixed the msg

--- src/helpers/api/genRegisterMailHelper.ts
+++ src/helpers/api/genRegisterMailHelper.ts
@@ -1,38 +1,40 @@
 export const genRegisterMailHelper = (name: string) => {
     return `<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                    <title>Register</title>
-                </head>
-                <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wellcome</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        nav {
+            width: 100%;
+            background: #f39c12;
+            color: white;
+            font-size: 20px;
+            text-align: center;
+            padding: 1em;
+            margin-bottom: 2em;
+        }
 
-                <nav class="navbar navbar-dark bg-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand">WELLCOME</a>
-                    </div>
-                </nav>
+        .text {
+            text-align: center;
+            font-size: 40px;
+        }
 
-                <div class="container">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="card-title">
-                                Wellcome to DressU
-                            </div>
-                            <div class="card-text">
-                                
-                            ${name}, THANK YOU SO MUCH FOR JOINING US!
-
-                            From today you will be part of our great family passionate about good clothes.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    
-                </body>
-                </html>`;
+    </style>
+</head>
+<body>
+    
+    <div class="text">
+        ${name}, THANK YOU SO MUCH FOR JOINING US!
+        
+        From today you will be part of our great family passionate about good clothes.
+    </div>
+</body>
+</html>`;
 };
