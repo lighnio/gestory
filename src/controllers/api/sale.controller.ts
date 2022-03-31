d57546e66eeaebe013c7bfeb5c1c5b6f4810f018
ripterdust <bryantello2010@hotmail.com>
2022-03-31 12:43:39 -0600

    Added the helper

--- src/controllers/api/sale.controller.ts
+++ src/controllers/api/sale.controller.ts
@@ -1,25 +1,12 @@
 import { Request, Response } from 'express';
 import PDFDocument from 'pdfkit';
 import { connection } from '../../database/db';
-
-interface productType {
-    id: string;
-}
+import { createSaleQuery } from '../../helpers/api/createSaleQuery';
 
 class Sales {
     store(req: Request, res: Response) {
         const { products } = req.body;
-
-        const fields = 'productName, productPrice, purchasePrice';
-        let query = `SELECT ${fields} FROM products where`;
-
-        products.map((product: productType, index: number) => {
-            const { id } = product;
-            query += `${
-                index != 0 ? ' OR' : ''
-            } BIN_TO_UUID(idProduct) = '${id}'`;
-        });
-
+        const query = createSaleQuery(products);
         res.status(200).send(query);
     }
 }
---
+++ src/helpers/api/createSaleQuery.ts
@@ -0,0 +1,16 @@
+interface productType {
+    id: string;
+}
+export const createSaleQuery = (products: Array<productType>) => {
+    const fields = 'productName, productPrice, purchasePrice';
+    let query = `SELECT ${fields} FROM products where`;
+
+    products.map((product: productType, index: number) => {
+        const { id } = product;
+        query += `${index != 0 ? ' OR' : ''} BIN_TO_UUID(idProduct) = '${id}'`;
+    });
+
+    query += ';';
+
+    return query;
+};
