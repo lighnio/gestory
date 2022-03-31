26fff762b366a661db7dc794f3374c7af5a55ef7
ripterdust <bryantello2010@hotmail.com>
2022-03-31 12:37:42 -0600

    Finished the query

--- src/controllers/api/sale.controller.ts
+++ src/controllers/api/sale.controller.ts
@@ -2,11 +2,25 @@ import { Request, Response } from 'express';
 import PDFDocument from 'pdfkit';
 import { connection } from '../../database/db';
 
+interface productType {
+    id: string;
+}
+
 class Sales {
     store(req: Request, res: Response) {
-        const doc = new PDFDocument();
-        doc.addPage();
-        res.send('hola');
+        const { products } = req.body;
+
+        const fields = 'productName, productPrice, purchasePrice';
+        let query = `SELECT ${fields} FROM products where`;
+
+        products.map((product: productType, index: number) => {
+            const { id } = product;
+            query += `${
+                index != 0 ? ' OR' : ''
+            } BIN_TO_UUID(idProduct) = '${id}'`;
+        });
+
+        res.status(200).send(query);
     }
 }
 
