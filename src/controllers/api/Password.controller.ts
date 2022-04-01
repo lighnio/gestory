00ba21a1e0897ab6d147b6c54319607b1552dbce
ripterdust <bryantello2010@hotmail.com>
2022-04-01 14:11:16 -0600

    Added the basic controllers

--- src/controllers/api/Password.controller.ts
+++ src/controllers/api/Password.controller.ts
@@ -1,3 +1,14 @@
-class Password {}
+import { Request, Response } from 'express';
+const bcryptjs: any = require('bcryptjs/dist/bcrypt');
+
+class Password {
+    edit(req: Request, res: Response) {
+        res.send('Change password');
+    }
+
+    patch(req: Request, res: Response) {
+        res.send('Password cambiado');
+    }
+}
 
 export const password = new Password();
