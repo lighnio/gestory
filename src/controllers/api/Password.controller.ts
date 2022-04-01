656211c4f56f2fdfe20f7d31ae432af1a1295d01
ripterdust <bryantello2010@hotmail.com>
2022-04-01 14:53:17 -0600

    Added the forgot api route

--- src/controllers/api/Password.controller.ts
+++ src/controllers/api/Password.controller.ts
@@ -1,10 +1,39 @@
 import { Request, Response } from 'express';
+import { connection } from '../../database/db';
+import { transporter } from '../../node-mailer/nodeMailer';
 const bcryptjs: any = require('bcryptjs/dist/bcrypt');
 
 class Password {
     edit(req: Request, res: Response) {
-        const { id } = req.params;
-        res.send('Change password');
+        const { mail } = req.body;
+        console.log(req.body);
+
+        const fields = 'BIN_TO_UUID(costumerId) as costumerId';
+        const query = `SELECT ${fields} FROM costumers WHERE costumerMail = "${mail}"`;
+        connection.query(query, (err, results) => {
+            if (err)
+                return res.status(500).send({
+                    err: true,
+                    msg: err.sqlMessage,
+                    query,
+                });
+
+            transporter.sendMail({
+                to: 'bryantello2010@hotmail.com',
+                from: mail,
+                subject: 'Reset password',
+                html: `<a href="www.google.com">Reset password</a>`,
+            });
+            res.status(200).send({
+                err: false,
+            });
+        });
+        // const mailConfig = {
+        //     from: 'Bryantello2010@hotmail.com',
+        //     to:
+        // };
+        // transporter.sendMail(mailConfig);
+        // res.send('Change password');
     }
 
     patch(req: Request, res: Response) {
