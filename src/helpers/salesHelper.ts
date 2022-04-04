d10697e1417a48b9f248f3f0aab2897b13721623
lighnio <charlesvas16@outlook.com>
2022-04-04 02:14:34 -0600

    add new value in return clausule

--- src/helpers/salesHelper.ts
+++ src/helpers/salesHelper.ts
@@ -6,17 +6,26 @@ export const salesHelper = (results: string[]) => {
         profitsSum,
         countSales,
         averageSum,
+        YearAvg,
         PromedioMaxM,
-        PromedioMinM,
+        PromedioMinM
     ] = JSON.parse(JSON.stringify(results));
 
     const [firstMax] = PromedioMaxM;
     const [firstMin] = PromedioMinM;
+    const [avgYear] = YearAvg;  
+
+    let percent = 0;
+    if(firstMax && firstMin && avgYear){
+        const subMax = (firstMax.MaxMAvg - firstMin.MinMAvg) / 100
+        percent = Math.ceil(avgYear.YearAvg / subMax)
+    }
+
     const [avgSum] = averageSum;
     const [profits] = profitsSum;
     const [total] = countSales;
     const { COUNT: count } = total;
 
     const sales = formateDateSalesHelper(salesUnformated);
-    return { sales, profits, count, avgSum,  firstMax, firstMin};
+    return { sales, profits, count, avgSum,  firstMax, firstMin, percent};
 };
