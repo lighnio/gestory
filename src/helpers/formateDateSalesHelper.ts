a7bfb35c29bed6c5221d8b8dee930c3ccc0ff741
ripterdust <bryantello2010@hotmail.com>
2022-04-05 14:11:47 -0600

    Restored the ticket a

--- src/helpers/formateDateSalesHelper.ts
+++ src/helpers/formateDateSalesHelper.ts
@@ -8,23 +8,14 @@ interface salesType {
 
 export const formateDateSalesHelper = (sales: Array<salesType>) => {
     return sales.map((sale: salesType) => {
-        const {
-            idSale,
-            saleProfit,
-            dateSale: date,
-            costumerId,
-            ticket: ticketBuffer,
-        } = sale;
+        const { idSale, saleProfit, dateSale: date, costumerId } = sale;
         const dateSale = new Date(date).toLocaleString('en-US');
 
-        const ticket = Buffer.from(ticketBuffer).toString('base64');
-        console.log(ticket);
         return {
             idSale,
             saleProfit,
             dateSale,
             costumerId,
-            ticket,
         };
     });
 };
