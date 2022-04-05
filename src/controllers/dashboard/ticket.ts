d12e76e942e1c29c69cb5abba34dbe0b5a610558
ripterdust <bryantello2010@hotmail.com>
2022-04-05 14:50:50 -0600

    Added the ticket controller

---
+++ src/controllers/dashboard/ticket.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { connection } from '../../database/db';
+class Ticket {
+    index(req: Request, res: Response) {
+        const query = 'SELECT ticket FROM sales;';
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            const ticket = Buffer.from(result).toString('base64');
+            console.log(ticket);
+            const resHeader = {
+                'Content-Disposition': `attachment; filename=${Date.now()}.pdf; content: ${ticket}`,
+                'Content-type': 'application/pdf',
+            };
+
+            res.header('Content-Type', 'application/pdf;base64');
+            res.end();
+        });
+    }
+}
+
+export const ticket = new Ticket();
